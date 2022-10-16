5d7e7b8d8568ab67ad4f131f28bd3862606e8f95
soyrockionic <44347792+soyrockionic@users.noreply.github.com>
2022-10-16 01:23:23 -0300

    Actualizo la clase

---
+++ Practica/Ejercicio14-LapsoDeTiempo/DateLapse2.java
@@ -0,0 +1,48 @@
+package ejercicio14.otraimplementacion;
+
+import ejercicio14.lapsodetiempo.Interface;
+import java.time.LocalDate;
+
+public class DateLapse2 implements Interface {
+    
+    private LocalDate from;
+    private int sizeInDays;
+	
+    public DateLapse2(LocalDate aDateFrom, int days) {
+	fromTo(aDateFrom,days);
+    }
+	
+    private void fromTo(LocalDate aDateFrom, int days) {
+	this.setFrom(aDateFrom);
+	this.setSizeInDays(days);
+    }
+	
+    public int sizeInDays() {
+	return this.getSizeInDays();
+    }
+	
+    public boolean includesDate(LocalDate other) {
+	LocalDate to = this.getFrom().plusDays(sizeInDays);
+	if(other.isAfter(getFrom()) && other.isBefore(to))
+	    return true;
+	else
+	    return false;
+    }
+
+    public LocalDate getFrom() {
+	return from;
+    }
+
+    public void setFrom(LocalDate from) {
+	this.from = from;
+    }
+
+    public int getSizeInDays() {
+        return sizeInDays;
+    }
+
+    public void setSizeInDays(int sizeInDays) {
+	this.sizeInDays = sizeInDays;
+    }
+        
+}
